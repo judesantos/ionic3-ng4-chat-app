52e1980c33bd61375e90066efad8eda188712a22
Jude Santos <jude@yourtechy.com>
2017-11-25 00:09:04 -0800

    remove firebase includes and providers

--- src/app/app.firebase.config.ts
+++
@@ -1,8 +0,0 @@
-export const FIREBASE_CONFIG = {
-  apiKey: "AIzaSyDMCUhoAv7Z1e4YRXYXBtk851_FZnL7YXg",
-  authDomain: "yourtechy-app.firebaseapp.com",
-  databaseURL: "https://yourtechy-app.firebaseio.com",
-  projectId: "yourtechy-app",
-  storageBucket: "yourtechy-app.appspot.com",
-  messagingSenderId: "543545300234"
-}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,8 @@ import { ErrorHandler, NgModule } from '@angular/core'
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular'
 import { SplashScreen } from '@ionic-native/splash-screen'
 import { StatusBar } from '@ionic-native/status-bar'
-import { AngularFireModule } from 'angularfire2'
-import { AngularFireAuthModule } from 'angularfire2/auth'
-import { AngularFireDatabaseModule } from 'angularfire2/database'
 
 import { MyApp } from './app.component'
-import { FIREBASE_CONFIG } from './app.firebase.config'
-import { AuthProvider } from '../providers/auth/auth';
-import { DataProvider } from '../providers/data/data';
 
 @NgModule({
   declarations: [
@@ -19,10 +13,6 @@ import { DataProvider } from '../providers/data/data';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
-    AngularFireModule.initializeApp(FIREBASE_CONFIG),
-    AngularFireAuthModule,
-    AngularFireDatabaseModule
-
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -32,8 +22,6 @@ import { DataProvider } from '../providers/data/data';
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
-    AuthProvider,
-    DataProvider
   ]
 })
 export class AppModule {}
